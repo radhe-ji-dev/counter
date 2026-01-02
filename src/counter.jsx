3929662e732c25689d026892bf7e692330e5c1d3
Radhakant Tiwadi <69426689+radhe-ji-dev@users.noreply.github.com>
2026-01-02 15:00:08 +0000

    test done

--- src/counter.jsx
+++ src/counter.jsx
@@ -4,7 +4,7 @@ function Counter() {
   const [count, setCount] = React.useState(0);
   return (
     <div>
-      <h2>Counter: {count}</h2>
+      <h2>Count: {count}</h2>
       <button onClick={() => setCount(count + 1)}>Increment</button>
 
       <button onClick={() => setCount(count - 1)}>Decrement</button>
